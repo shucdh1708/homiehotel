361ffac701080f4fa3098e4b0b8b378cb0271922
unknown <cdhuy01202@gmai.com>
2022-11-11 21:50:42 +0700

    sc

--- src/main/java/thuctap/services/GiaoDienService.java
+++
@@ -1,9 +0,0 @@
-package thuctap.services;
-
-import org.springframework.data.repository.CrudRepository;
-
-import thuctap.model.Display;
-
-public interface GiaoDienService extends CrudRepository<Display, Integer> {
-
-}
--- src/main/java/thuctap/services/ThongSoTheoGioService.java
+++
@@ -1,9 +0,0 @@
-package thuctap.services;
-
-import org.springframework.data.repository.CrudRepository;
-
-import thuctap.model.HourlyParameters;
-
-public interface ThongSoTheoGioService extends CrudRepository<HourlyParameters, Integer> {
-
-}
